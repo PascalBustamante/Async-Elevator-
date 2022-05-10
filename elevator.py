5e41832fe99f96f547079e30eb52dabf9e8f51a2
PascalBustamante <57356405+PascalBustamante@users.noreply.github.com>
2022-05-10 08:04:55 -0500

    Delete elevator.py

--- elevator.py
+++
@@ -1,5 +0,0 @@
-class Elevator(object):
-    def __init__(self):
-        self.currentFloor = 0
-        self.direction = 1 #1 = up, -1 = down (maybe i can get tid of 0 and only keep the old direction)
-        self.destination = 0 
